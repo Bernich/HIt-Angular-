f6e3a25b8ab72dea145c6a951f746ab773a538bd
botchway44 <noelnuel44@gmail.com>
2021-05-11 11:09:52 +0000

    [WIP] +> cleanup on the ssr pages

--- config/app.js
+++ config/app.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(
-  express.static(path.join(__dirname, "academy-app"), {
+  express.static(path.join(__dirname, "hive-media"), {
     redirect: "/",
     index: "index.html",
   })
