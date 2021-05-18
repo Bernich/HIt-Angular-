b2a266480a1a83a4d62c77231295eace8f4541bd
botchway44 <noelnuel44@gmail.com>
2021-05-18 18:04:32 +0000

    [WIP] +> cleanup on convince and news

--- src/app/layouts/news/news.component.ts
+++ src/app/layouts/news/news.component.ts
@@ -14,16 +14,21 @@ export class NewsListComponent implements OnInit {
 
   news = [
     {
-      image: 'https://prodcmscdn.azureedge.net/careerconnectresources/p/MICRUS/images/studentshomehero-1568023329563.jpg',
-      title: 'Learn a skill and earn a living'
+      image: 'https://news.harvard.edu/wp-content/uploads/2021/05/ART_Jagged-Little-Pill_EE-2500-1200x800.jpg',
+      title: 'A.R.T. maintains global collaborations, with technology and remote coordination',
+      description: 'The popular A.R.T. Travels events will continue as a virtual series this spring, but the theater is making preparations for some upcoming, in-person performances. And it has some pertinent pandemic-era experience under its belt.'
     },
     {
-      image: 'https://prodcmscdn.azureedge.net/careerconnectresources/p/MICRUS/images/studentshomehero-1568023329563.jpg',
-      title: 'This is a Second course program'
+      image: 'https://news.harvard.edu/wp-content/uploads/2021/05/iStock-1211101305-900x600.jpg',
+      title: 'Why some die, some survive when equally ill from COVID-19',
+      description: 'We were interested in asking whether we could identify mechanisms that might be contributing to death in COVID-19,” says MGH infectious disease expert Marcia Goldberg, who studies interactions between microbial pathogens and their hosts, and is senior author of the study. '
+
     },
     {
-      image: 'https://prodcmscdn.azureedge.net/careerconnectresources/p/MICRUS/images/studentshomehero-1568023329563.jpg',
-      title: 'This is a Third course program'
+      description: 'A year later, the Gazette caught up with Waldo, Gordon McKay Professor of the Practice of Computer Science and the Paulson School’s chief technology officer, to see how it went.',
+      title: 'High-speed internet at a crossroads',
+      image: 'https://engineering.stanford.edu/sites/default/files/styles/full_width_banner_tall/public/20210312-tfoe-jurafsky-stocksy_txp556a7c75o01300_large_2512241.jpg?itok=iCkNp7ty'
+
     },
   ]
   ngOnInit() { }
--- src/app/layouts/programs/programs.component.ts
+++ src/app/layouts/programs/programs.component.ts
@@ -14,16 +14,16 @@ export class ProgramsComponent implements OnInit {
 
   programs = [
     {
-      image: 'https://prodcmscdn.azureedge.net/careerconnectresources/p/MICRUS/images/studentshomehero-1568023329563.jpg',
-      title: 'This is a course program'
+      image: 'https://engineering.stanford.edu/sites/default/files/styles/full_width_banner_tall/public/20210512-niac-pavone_graphic_2021_ph_i.jpg?itok=MB7dg5cJ',
+      title: 'Modern Computer Science'
     },
     {
-      image: 'https://prodcmscdn.azureedge.net/careerconnectresources/p/MICRUS/images/studentshomehero-1568023329563.jpg',
-      title: 'This is a Second course program'
+      image: 'https://engineering.stanford.edu/sites/default/files/styles/full_width_banner_tall/public/20210423-tfoe-agrawala-adobestock_361542557.jpg?itok=306hNN2c',
+      title: 'Business Management Essential'
     },
     {
-      image: 'https://prodcmscdn.azureedge.net/careerconnectresources/p/MICRUS/images/studentshomehero-1568023329563.jpg',
-      title: 'This is a Third course program'
+      image: 'https://engineering.stanford.edu/sites/default/files/styles/6-col-banner-short/public/20210422-floodwaters-istock-1199361013.jpg?itok=5aKHBou5',
+      title: 'Modern app development with Flutter'
     },
   ]
   ngOnInit() { }
