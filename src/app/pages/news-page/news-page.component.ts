b7259ed8529767abed6efdf82167174474d40e62
botchway44 <noelnuel44@gmail.com>
2021-06-04 13:30:24 +0000

    cleanups

--- src/app/pages/news-page/news-page.component.ts
+++ src/app/pages/news-page/news-page.component.ts
@@ -13,23 +13,23 @@ export class NewsPageComponent implements OnInit {
 
 
 
-  headerPosts = [
-    {
-      image: 'https://www.stanford.edu/wp-content/uploads/2020/09/Academics-landing.jpg',
-      title: 'A culture where everyone can thrive',
-      description: 'We are focused on creating a vibrant community of belonging that pursues diversity, builds inclusion, and creates more equitable opportunities for learning.'
-    },
-    {
-      image: 'https://www.stanford.edu/wp-content/uploads/2017/03/hoover-background-lg-1499x552.jpg',
-      title: 'Hive Institute is shaped and strengthened by our many communities.',
-      description: 'We encourage our students to explore the academic landscape, venturing into unfamiliar fields of knowledge and, perhaps, discovering new passions that will take them in a different direction altogether.'
-    },
-    {
-      image: 'https://www.stanford.edu/wp-content/uploads/2019/01/fac-staff-hero-landscape-1499x522.jpg',
-      title: 'People committed to public purpose',
-      description: 'Rich learning experiences that provide a broad liberal arts foundation and deep subject-area expertise. Unsurpassed opportunities to participate in the advancement of entire fields of knowledge'
-    }
-  ];
+  // headerPosts = [
+  //   {
+  //     image: 'https://www.stanford.edu/wp-content/uploads/2020/09/Academics-landing.jpg',
+  //     title: 'A culture where everyone can thrive',
+  //     description: 'We are focused on creating a vibrant community of belonging that pursues diversity, builds inclusion, and creates more equitable opportunities for learning.'
+  //   },
+  //   {
+  //     image: 'https://www.stanford.edu/wp-content/uploads/2017/03/hoover-background-lg-1499x552.jpg',
+  //     title: 'Hive Institute is shaped and strengthened by our many communities.',
+  //     description: 'We encourage our students to explore the academic landscape, venturing into unfamiliar fields of knowledge and, perhaps, discovering new passions that will take them in a different direction altogether.'
+  //   },
+  //   {
+  //     image: 'https://www.stanford.edu/wp-content/uploads/2019/01/fac-staff-hero-landscape-1499x522.jpg',
+  //     title: 'People committed to public purpose',
+  //     description: 'Rich learning experiences that provide a broad liberal arts foundation and deep subject-area expertise. Unsurpassed opportunities to participate in the advancement of entire fields of knowledge'
+  //   }
+  // ];
 
 
 
