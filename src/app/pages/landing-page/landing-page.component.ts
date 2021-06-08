26e06e74f1ed838d40cc8b8c1cc43661f1ee3099
Bernich <berni24695@gmail.com>
2021-06-08 16:33:32 +0000

    clean ups done to the Authors Bio

--- src/app/pages/landing-page/landing-page.component.ts
+++ src/app/pages/landing-page/landing-page.component.ts
@@ -20,21 +20,39 @@ export class LandingPageComponent implements OnInit {
   // People committed to public purpose
   // Harvard is shaped and strengthened by our many communities.
   //A Hive Institute education is not a narrow path that students follow from Point A to Point Z.
+  // headerPosts = [
+  //   {
+  //     image: 'https://www.stanford.edu/wp-content/uploads/2020/09/Academics-landing.jpg',
+  //     title: 'A culture where everyone can thrive',
+  //     description: 'We are focused on creating a vibrant community of belonging that pursues diversity, builds inclusion, and creates more equitable opportunities for learning.'
+  //   },
+  //   {
+  //     image: 'https://www.stanford.edu/wp-content/uploads/2017/03/hoover-background-lg-1499x552.jpg',
+  //     title: 'Hive Institute is shaped and strengthened by our many communities.',
+  //     description: 'We encourage our students to explore the academic landscape, venturing into unfamiliar fields of knowledge and, perhaps, discovering new passions that will take them in a different direction altogether.'
+  //   },
+  //   {
+  //     image: 'https://www.stanford.edu/wp-content/uploads/2019/01/fac-staff-hero-landscape-1499x522.jpg',
+  //     title: 'People committed to public purpose',
+  //     description: 'Rich learning experiences that provide a broad liberal arts foundation and deep subject-area expertise. Unsurpassed opportunities to participate in the advancement of entire fields of knowledge'
+  //   }
+  // ];
+
   headerPosts = [
     {
       image: 'https://www.stanford.edu/wp-content/uploads/2020/09/Academics-landing.jpg',
-      title: 'A culture where everyone can thrive',
-      description: 'We are focused on creating a vibrant community of belonging that pursues diversity, builds inclusion, and creates more equitable opportunities for learning.'
+      title: 'Set yourself apart',
+      description: 'We give you the necessary knowledge to be a change leader in your sphere of influence.'
     },
     {
       image: 'https://www.stanford.edu/wp-content/uploads/2017/03/hoover-background-lg-1499x552.jpg',
-      title: 'Hive Institute is shaped and strengthened by our many communities.',
-      description: 'We encourage our students to explore the academic landscape, venturing into unfamiliar fields of knowledge and, perhaps, discovering new passions that will take them in a different direction altogether.'
+      title: 'Get caught up with the latest in tech.',
+      description: 'Be exposed to new research and technology as a student that would spark your creativity.'
     },
     {
       image: 'https://www.stanford.edu/wp-content/uploads/2019/01/fac-staff-hero-landscape-1499x522.jpg',
-      title: 'People committed to public purpose',
-      description: 'Rich learning experiences that provide a broad liberal arts foundation and deep subject-area expertise. Unsurpassed opportunities to participate in the advancement of entire fields of knowledge'
+      title: 'Get ahead of your peers',
+      description: 'Our unconventional facilitation style will psychologically motivate you as you leverage on the skills of our seasoned trainers.'
     }
   ];
 
